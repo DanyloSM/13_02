85f9e563fe945cc5a9f85ddeb12a36202ae734ee
danylo <tortik24092002@gmail.com>
2026-02-13 09:37:41 +0100

    initial commit

---
+++ .venv/dz1.py
@@ -0,0 +1,25 @@
+print('Hello World!')
+age = 35
+print(id(age))
+y = 335
+z = y
+print(id(z))
+my_name = 'Danylo'
+print(id(my_name))
+print(type(my_name))
+print(11 // 3)
+print(11 % 3)
+divmod(-11,  3)
+print(18 % 9)
+print(18 / 9)
+a = 5
+b = 10
+c = a + b * 3
+print(c)
+#279
+
+
+
+
+
+
