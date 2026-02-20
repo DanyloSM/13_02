571bed456efc6239c3b768bd4f53774b40c784f9
danylo <tortik24092002@gmail.com>
2026-02-20 09:26:10 +0100

    dz3.2 redo

--- DZ3/dz3.2.py
+++ DZ3/dz3.2.py
@@ -1,15 +1,8 @@
 list1 = [12, 3, 4, 10] #=> [10, 12, 3, 4]
-#last = list1.pop()
-#list1.insert(0, last)
-#print(list1)
 #list1 = [1]#=> [1]
-#last = list1.pop()
-#list1.insert(0, last)
-#print(list1)
-#list1 = []
-#print(list1[-1:] + list1[:-1]) #=> []
-#list1 = [12, 3, 4, 10, 8]
-#print(list1[-1:] + list1[:-1]) #=> [8, 12, 3, 4, 10]
+#list1 = [] #=> []
+#list1 = [12, 3, 4, 10, 8] #=> [8, 12, 3, 4, 10]
+print(list1[-1:] + list1[:-1])
 
 
 
